dae60cac5412650219f5a3cb7bd93024f3147b70
Jonas Sandstedt <kontakta@jonassandstedt.se>
2019-07-05 10:02:32 +0200

    Updated the Fully logo animation
    Kudos to Emil Gradén for recreating the animation!

--- src/assets/_javascripts/components/LogoAnimation/index.js
+++ src/assets/_javascripts/components/LogoAnimation/index.js
@@ -1,33 +1,34 @@
 import lottie from 'lottie-web';
 
-module.exports =
-class LogoAnimation {
+module.exports = class LogoAnimation {
 	constructor () {
 		this._logoContainer = document.getElementById('logo_anim');
-		this._path = '/assets/logo.json';
+		this._path = '/assets/fully_logo_singel.json';
 
-		// hide logoanim on Windows Edge because it looks like shit with the masks inside
-		if (window.navigator.userAgent.indexOf("Edge") > -1) {
-			this._logoContainer.classList.add('fallback');
-			return;
-		}
+		// // hide logoanim on Windows Edge because it looks like shit with the masks inside
+		// if (window.navigator.userAgent.indexOf("Edge") > -1) {
+		// 	this._logoContainer.classList.add('fallback');
+		// 	return;
+		// }
 
-        this._animData = {
-        	container: this._logoContainer,
-        	renderer: 'svg',
-        	loop: false,
+		this._animData = {
+			container: this._logoContainer,
+			renderer: 'svg',
+			loop: false,
 			autoplay: false,
 			path: this._path
-        }
+		}
 
-        this._anim = lottie.loadAnimation (this._animData);
-        this._anim.addEventListener ('DOMLoaded', this.startLogoAnimation.bind(this));
+		this._anim = lottie.loadAnimation(this._animData);
+		this._anim.addEventListener('DOMLoaded', this.startLogoAnimation.bind(this));
 
-        lottie.setQuality(2);
+		lottie.setQuality(2);
 		window.onresize = this._anim.resize.bind(this._anim);
-    }
+	}
 
-    startLogoAnimation () {
-    	this._anim.goToAndPlay (1, true);
-    }
+	startLogoAnimation () {
+		setTimeout(() => {
+			this._anim.play();
+		}, 600);
+	}
 }
