9c20750087103480b9ca07e754d55bc5404c5eb8
Jonas Sandstedt <git@jonassandstedt.se>
2020-06-10 15:36:37 +0200

    cleaned up heroanimation

--- src/_js/components/Loadinghero/index.js
+++ src/_js/components/Loadinghero/index.js
@@ -4,12 +4,24 @@ var initLoadingHero = function(){
   document.body.classList.add('content-loaded');
 
   const animLength = 3;
-  const heading = gsap.to('#loadinghero-header', { duration: 0.6, delay: animLength, opacity: 0, y: -500, ease: "back.in(1.3)" });
-  const wrapper = gsap.to('.loadinghero', { duration: 0.6, delay: animLength + 0.4, height: 0, ease: "power4.easeOut" });     
+  const heading = gsap.to('#loadinghero-header', {
+    duration: 0.6,
+    delay: animLength,
+    opacity: 0,
+    y: -500,
+    ease: "back.in(1.3)"
+  });
+  const wrapper = gsap.to('.loadinghero', {
+    duration: 0.6,
+    delay: animLength + 0.4,
+    height: 0,
+    ease: "power4.easeOut",
+    onComplete: () => { document.querySelector('.loadinghero').remove(); }
+  });     
   heading.play();
   wrapper.play();
 }
 
-document.querySelector('.loadinghero').addEventListener('load', (event) => {
+window.addEventListener('load', (event) => {
   initLoadingHero();
 });
