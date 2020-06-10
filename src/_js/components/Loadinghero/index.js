f6da38ad2f91cd8e3b5941a8b6bd27c7510556d5
Jonas Sandstedt <git@jonassandstedt.se>
2020-06-10 15:22:42 +0200

    Bugfixes heroanimation

--- src/_js/components/Loadinghero/index.js
+++ src/_js/components/Loadinghero/index.js
@@ -1,26 +1,15 @@
 import gsap from 'gsap';
 
-function ready(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-      document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
 var initLoadingHero = function(){
   document.body.classList.add('content-loaded');
-  var tl = gsap.timeline({delay: 0.5});
-  var animLength = 3;
-  tl
-    .add('out', animLength)
-    .to('.loadinghero__heading', 0.6, { opacity: 0, y: '-5vh', delay: 0, ease: "back.in(3)" }, 'out')
-    .to('.loadinghero', 0.4, {height: 0, ease: "power4.easeOut", delay: 0.7}, 'out');     
 
-  tl.eventCallback("onComplete", () => {
-    document.querySelector('.loadinghero').remove();
-    // document.querySelectorAll('.loadinghero__video').forEach(vid => vid.pause());
-  });
+  const animLength = 3;
+  const heading = gsap.to('#loadinghero-header', { duration: 0.6, delay: animLength, opacity: 0, y: -500, ease: "back.in(1.3)" });
+  const wrapper = gsap.to('.loadinghero', { duration: 0.6, delay: animLength + 0.4, height: 0, ease: "power4.easeOut" });     
+  heading.play();
+  wrapper.play();
 }
 
-ready(initLoadingHero);
+document.querySelector('.loadinghero').addEventListener('load', (event) => {
+  initLoadingHero();
+});
